4f667397c780ba90f8da2995c389785b141c3007
Lucas Menke <lucas_menke@icloud.com>
2023-01-18 22:52:06 +0100

    style changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import Header from "./Header"
+import Header from "./Header";
 
 export default function RootLayout({
   children,
@@ -9,9 +9,9 @@ export default function RootLayout({
   return (
     <html>
       <head />
-      <body>
+      <body className="bg-gray-200 dark:bg-zinc-900 transition-all duration-700">
         <Header />
-        <div>{children}</div>
+        <div className="max-w-6xl mx-auto">{children}</div>
       </body>
     </html>
   );
